00261c2f3c7542b7000ad72286168fc88ab8e118
Mohamed Ismail <darkangel7863@gmail.com>
2026-08-27 22:30:45 +0300

    Dividing the components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return <div>Logo</div>;
+}
+function Form() {
+  return <div>Form</div>;
+}
+function PackingList() {
+  return <div>PackingList</div>;
+}
+function Stats() {
+  return <div>Stats</div>;
+}
