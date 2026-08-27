5cee65f5b5690b29693c17e1bbf796438c760301
Mohamed Ismail <darkangel7863@gmail.com>
2026-08-27 23:52:23 +0300

    Building the packing List

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+const initialItems = [
+  { id: 1, description: 'Passports', quantity: 2, packed: false },
+  { id: 2, description: 'Socks', quantity: 12, packed: false },
+  { id: 3, description: 'charger', quantity: 1, packed: true },
+];
+
 export default function App() {
   return (
-    <div>
+    <div className="app">
       <Logo />
       <Form />
       <PackingList />
@@ -10,14 +16,41 @@ export default function App() {
 }
 
 function Logo() {
-  return <div>Logo</div>;
+  return <h1>🌴 Far Away 👜</h1>;
 }
 function Form() {
-  return <div>Form</div>;
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
 }
 function PackingList() {
-  return <div>PackingList</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map(item => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 function Stats() {
-  return <div>Stats</div>;
+  return (
+    <footer className="stats">
+      <em>💼 You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
 }
